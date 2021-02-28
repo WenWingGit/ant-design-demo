df44e035ebf5fec25a6a7464fb0593a89ea1514a
git1997 <5241386+JIeJamey@user.noreply.gitee.com>
2021-02-28 16:55:59 +0800

    自定义toast

--- src/plugins/mixin.js
+++ src/plugins/mixin.js
@@ -36,6 +36,38 @@ export default Vue => {
             }
           });
         })
+      },
+      $sx_toast(toastText) {
+        let body = document.body
+        let children = document.createElement("div")
+        let html = `<div class="toast">${toastText}</div>
+          <style>.toast {
+            position: fixed;
+            z-index: 2000;
+            left: 50%;
+            top:45%;
+            transition:all .5s;
+            -webkit-transform: translateX(-50%) translateY(-50%);
+               -moz-transform: translateX(-50%) translateY(-50%);
+                -ms-transform: translateX(-50%) translateY(-50%);
+                 -o-transform: translateX(-50%) translateY(-50%);
+                    transform: translateX(-50%) translateY(-50%);
+            text-align: center;
+            border-radius: 5px;
+            color:#FFF;
+            background: rgba(17, 17, 17, 0.7);
+            height: 45px;
+            line-height: 45px;
+            padding: 0 15px;
+            max-width: 150px;
+        }</style>
+        `
+        children.innerHTML = html
+        body.appendChild(children)
+        let timer = setTimeout(function(){
+          body.removeChild(children)
+          clearTimeout(timer)
+        }, 1500)
       }
     },
   })
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     商品列表
+    <hr>
+    <button @click="showToast">显示自定义Toast</button>
   </div>
 </template>
 
 <script>
   export default {
     name: "GoodsList",
+    methods: {
+      showToast() {
+        this.$sx_toast('我现在显示自定义Toast啦')
+      }
+    }
   }
 </script>
 
