0fdfe882ee5bc89986e1936da61d752ad5a8bed4
git1997 <5241386+JIeJamey@user.noreply.gitee.com>
2021-03-12 23:24:18 +0800

    scss _var 不需要再引入

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/test/' 
+    ? '/test/'
     : '/',
   lintOnSave: 'warning', // 保存代码时，eslint仅警告而不报错
   devServer: {
@@ -18,17 +18,17 @@ module.exports = {
   },
   css: {
     loaderOptions: {
-        sass: {
-            prependData: `@import "./src/assets/scss/_var";`,
-        },
+      sass: {
+        prependData: `@import "./src/assets/scss/_var";`,
+      },
     },
   },
   // 链式操作webpack，在默认配置上进行修改
   chainWebpack: config => {
     // 配置别名
     config.resolve.alias
-    .set('@', path.resolve(__dirname, 'src'))
-    .set('~c', path.resolve(__dirname, 'src', 'components'))
-    .set('~v', path.resolve(__dirname, 'src', 'views'))
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('~c', path.resolve(__dirname, 'src', 'components'))
+      .set('~v', path.resolve(__dirname, 'src', 'views'))
   }
 }
